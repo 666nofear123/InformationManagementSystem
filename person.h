633284a46df3a925b0003f580b7927f0f2bef313
666nofear123 <1324620551@qq.com>
2025-05-22 17:49:43 +0800

    Update person.h

--- person.h
+++ person.h
@@ -6,14 +6,24 @@ using namespace std;
 
 class Person {
 public:
+    Person();
     Person(string i, string n, string g, string id_C, string p);
+
    virtual ~Person();
-   virtual void display();
+
+   void setId(string i);
+   void setName(string n);
+   void setGender(string g);
+   void setIdCard(string id_C);
+   void setPhone(string p);
+
    string getId() const;
    string getName() const;
    string getGender() const;
    string getIdCard() const;
    string getPhone() const;
+   
+   virtual void display();
    
 protected:   //子类的成员函数会调用这些量，所以设为protected
     string id;
