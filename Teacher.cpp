61fe6ad8ce3d6d17ebe832f5e6f04f82cb49b77c
999668 <2507836018@qq.com>
2025-05-25 16:19:13 +0800

    Update Teacher.cpp

--- Teacher.cpp
+++ Teacher.cpp
@@ -1,20 +1,25 @@
-#include <iostream>
 #include "Person.h"
-#include <string>
+#include "Teacher.h"
+#include <iostream>
 using namespace std;
 
 
     Teacher::Teacher()
     {
-    joinYear=0;
-    course=" ";
+       joinYear=0;
+       course=" ";
     }
     Teacher::Teacher(string i, string n, string g, string id_C, string p, int jY, string c)
-    :Persion::persion(i,n,g,c,p)
     {
-    setJoinYear(jY);
-    setCourse(c);
+        setId(i);
+        setName(n);
+        setGender(g);
+        setIdCard(id_C);
+        setPhone(p);
+        setJoinYear(jY);
+        setCourse(c);
     }
+
     Teacher::~Teacher()
     {
 
@@ -22,11 +27,11 @@ using namespace std;
 
     void Teacher::setJoinYear(int jY)
     {
-     joinYear=jY;
+         joinYear=jY;
     }
     void Teacher::setCourse(string c)
     {
-    course=c;
+         course=c;
     }
 
     int Teacher::getJoinYear() const
@@ -40,6 +45,12 @@ using namespace std;
 
     void Teacher::display()
     {
-    cout<<"入职年份"<<joinyear<<endl;
-    cout<<"所授课程"<<course<<endl;
+    cout << "\n教师信息：\n"
+             << "学工号：" << id << "\n"
+             << "姓名：" << name << "\n"
+             << "性别：" << gender << "\n"
+             << "入职年份：" << joinYear << "\n"
+             << "所教课程："<< course << "\n"
+             << "身份证号：" << id_Card << "\n"
+             << "手机号：" << phone << endl;
     }
