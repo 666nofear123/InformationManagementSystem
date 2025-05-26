1fff2c132f46a265b2fd9dcdda18bf94f5be4b99
666nofear123 <1324620551@qq.com>
2025-05-26 21:45:39 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,14 +3,22 @@
 3.学生成绩管理功能 在学生的信息已经添加的基础之上，将学生的一个学期所学课程的期末成绩添加入其中，并计算出平均成绩。  
 结果展示:当通过查询面板输入教师的学工号时，显示出教师的相关信息。当输入学生的信息时，显示出学生的相关信息以及各科成绩和平均成绩
 */
-/*使用技术：封装、继承、多态、文件读写、STL容器等
+/*使用技术：封装、继承、多态、STL容器等
 */
 #include <iostream>
 #include <string>
 #include <vector>
 #include <fstream>
 
+#include "Person.cpp"
+#include "Student.cpp"
+#include "Teacher.cpp"
+#include "ManagementSystem.cpp"
+
 using namespace std;
 int main(){
-    
+    ManagementSystem m;
+    m.showMenu();
+
+    return 0;
 }
